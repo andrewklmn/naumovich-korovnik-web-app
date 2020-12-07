0d0f15087bc0a642b9350c6ef1b050504a71c652
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-12-07 15:46:41 +0200

    Add: config json backend save function

--- set_config.php
+++ set_config.php
@@ -1,6 +1,24 @@
 <?php
 
-    //header('Content-Type: text/plain');
-
-    //echo file_get_contents('/var/log/'.$_REQUEST['name']);
+    header('Content-Type: text/plain');
+    
+    if (isset($_REQUEST[config])) {
+        $params = explode('|', $_REQUEST[config]);
+        if (count($params) == 5) {
+            $new_config = array(
+                "dayTime" => $params[0],
+                "dayTemp" => $params[1],
+                "nightTime" => $params[2],
+                "nightTemp" => $params[3],
+                "weekendTemp" => $params[4]
+            );
+            
+            file_put_contents('/var/www/html/naumovich/config.json', json_encode($new_config));
+            
+            echo 'New config is setted';
+        }
+        echo 'Wrong config!';
+    }
+    
+    echo 'No config!';
     
