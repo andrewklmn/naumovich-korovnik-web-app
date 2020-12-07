32d55f4d7875cf4a7047be1f992d760287cedc43
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-12-07 16:02:22 +0200

    Update: config json backend save function

--- set_config.php
+++ set_config.php
@@ -2,22 +2,25 @@
 
     header('Content-Type: text/plain');
     
-    if (isset($_REQUEST[config])) {
-        $params = explode('|', $_REQUEST[config]);
+    if (isset($_REQUEST['config'])) {
+        $params = explode('|', $_REQUEST['config']);
+        
         if (count($params) == 5) {
-            $new_config = array(
-                "dayTime" => $params[0],
-                "dayTemp" => $params[1],
-                "nightTime" => $params[2],
-                "nightTemp" => $params[3],
-                "weekendTemp" => $params[4]
-            );
+            $new_config = '{
+                "dayTime": "'.$params[0].'",
+                "dayTemp": "'.$params[1].'",
+                "nightTime": "'.$params[2].'",
+                "nightTemp": "'.$params[3].'",
+                "weekendTemp": "'.$params[4].'"
+}';
             
-            file_put_contents('/var/www/html/naumovich/config.json', json_encode($new_config));
+            file_put_contents('/var/www/html/naumovich/config.json', $new_config);
             
             echo 'New config is setted';
+            exit;
         }
         echo 'Wrong config!';
+        exit;
     }
     
     echo 'No config!';
